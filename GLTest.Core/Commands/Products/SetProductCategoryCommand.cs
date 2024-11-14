60fb6cba8ce851ac5e7409ad89f367af23fb17d8
Chelsea Cruz <33120739+carloluisito@users.noreply.github.com>
2024-11-14 23:18:35 +0800

    Add checking if the category is already linked into a product

--- GLTest.Core/Commands/Products/SetProductCategoryCommand.cs
+++ GLTest.Core/Commands/Products/SetProductCategoryCommand.cs
@@ -1,6 +1,8 @@
 ﻿using GLTest.Core.Common;
 using GLTest.Core.Repositories.Categories;
 using GLTest.Core.Repositories.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace GLTest.Core.Commands.Products
 {
@@ -21,13 +23,18 @@ namespace GLTest.Core.Commands.Products
 
         public async Task<CommandResult<bool>> ExecuteAsync(Guid productId, Guid categoryId)
         {
-            var product = await _productRepository.FirstOrDefaultAsync(a => a.ProductId == productId);
+            var product = await _productRepository.FirstOrDefaultAsync(a => a.ProductId == productId, include: p => p.Include(c => c.Categories));
             if (product == null)
                 return new CommandResult<bool>("Set_Product_Category_Product_NotFound", "Product not found. ");
 
             var category = await _categoryRepository.FirstOrDefaultAsync(a => a.CategoryId == categoryId);
             if (category == null)
                 return new CommandResult<bool>("Set_Product_Category_Category_NotFound", "Category not found. ");
+
+            bool isAlreadyLinked = product.Categories.Any(category => category.CategoryId == categoryId);
+
+            if (isAlreadyLinked)
+                return new CommandResult<bool>("Set_Product_Category_Category_AlreadyLinked", "Category already linked in the product.");
 
             product.AddCategory(category);
 
